72ddeea9d4f97e77d6c581df09666ec790357f5b
ikarydi <ioanna.karydi@scout24.com>
2017-07-11 13:55:53 +0200

    wip adding header and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import logoScout24 from './../images/scout24.jpeg';
+import logoIS24 from './../images/is24.jpeg';
+import logoAS24 from './../images/as24.jpeg';
 import './Header.scss'
 const Header = () =>
   <div className="header">
@@ -16,9 +19,11 @@ const Header = () =>
       <div className="resp-header blue">
         <div className="page-wrapper">
           <h1 className="logo">
-            <a className="home-link" href='https://www.immobilienscout24.de'>
-              ImmobilienScout24
-            </a>
+            <div className="App-header">
+              <img src={logoScout24} className="App-logo" alt="logo" />
+              <img src={logoAS24} className="App-logo" alt="logo" />
+              <img src={logoIS24} className="App-logo" alt="logo" />
+            </div>
           </h1>
           <div className="main_menu_wrapper" />
         </div>
