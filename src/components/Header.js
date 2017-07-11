03670d10081f51816b99853d9ce9fa106e2650ca
ikarydi <ioanna.karydi@scout24.com>
2017-07-11 14:36:59 +0200

    wip adding header and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,11 @@
 import React from 'react'
-import logoScout24 from './../images/scout24.jpeg';
-import logoIS24 from './../images/is24.jpeg';
-import logoAS24 from './../images/as24.jpeg';
-import './Header.scss'
+import logoScout24 from './../images/scout24.png';
+import logoIS24 from './../images/is24.png';
+import logoAS24 from './../images/as24.png';
+import './Header.css'
 const Header = () =>
   <div className="header">
     <div id="scoutmanager-header">
-      <div className="above-header clearfix">
-        <div className="page-wrapper">
-          <div className="left">
-            {'\u00A0'}
-          </div>
-          <div className="right">
-            {'\u00A0'}
-          </div>
-        </div>
-      </div>
       <div className="resp-header blue">
         <div className="page-wrapper">
           <h1 className="logo">
@@ -24,8 +14,12 @@ const Header = () =>
               <img src={logoAS24} className="App-logo" alt="logo" />
               <img src={logoIS24} className="App-logo" alt="logo" />
             </div>
+            <div className="language-menu">
+              <a href="#">DE |</a>
+              <a href="#"> EN</a>
+            </div>
           </h1>
-          <div className="main_menu_wrapper" />
+
         </div>
       </div>
     </div>
