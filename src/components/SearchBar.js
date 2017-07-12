a7c91ab46aa61578d25d9055c62f4951d2e6922a
ikarydi <ioanna.karydi@scout24.com>
2017-07-12 11:08:15 +0200

    add textfield and dropdown to searchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import './cover.css';
+import './SearchBar.css'
 
 class SearchBar extends Component {
     render() {
         return (
             <div className="search-bar grid gutter-m padding-vertical-m padding-horizontal-s">
                 <div className="grid-item palm-one-whole lap-one-third desk-one-third">
-                    <select>
-                        <option>Job Title</option>
-                    </select>
+                    <input className="specialty" placeholder="I am searching for..."/>
                 </div>
                 <div className="grid-item palm-one-whole lap-one-third desk-one-third">
                     <select>
-                        <option>Job Level</option>
+                        <option>City</option>
+                        <option>Berlin</option>
+                        <option>Munich</option>
                     </select>
                 </div>
                 <div className="grid-item palm-one-whole lap-one-third desk-one-third">
