6f0e4ed3e6c5438749d7daeeb88ff9de8ad6f645
mkrey <maya.krey@immobilienscout24.de>
2017-07-13 14:06:08 +0200

    HAC-14: social icon

--- src/components/IconScout.js
+++ src/components/IconScout.js
@@ -3,8 +3,11 @@ import './IconCockpit.css';
 
 const IconScout = () => (
     <div className="grid-item palm-one-whole lap-one-third desk-one-third text-center">
-        <p className="font-l font-light">Scout24</p>
-        <p className="font-light">Who and what is Scout24</p>
+        <a href="https://walls.io/scout24" className="icon-link">
+            <i className="fa fa-user icons font-xxl" aria-hidden="true"/>
+            <p className="font-l font-light">Social</p>
+            <p className="font-light">Find out about our social activities</p>
+        </a>
     </div>
 );
 
