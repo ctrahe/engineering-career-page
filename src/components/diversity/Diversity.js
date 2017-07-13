03a5a8cd74e11755de347a50b95004675004ae70
Christin Westermann <christin.westermann@immobilienscout24.de>
2017-07-13 08:30:29 +0200

    HAC-14 added diversity claim

--- src/components/diversity/Diversity.js
+++ src/components/diversity/Diversity.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react';
 import GenderChart from './GenderChart';
 import { Translate } from 'react-i18nify';
 import diversityMap from './../../images/diversity.png'
+import Claim from './Claim';
 
 class Diversity extends Component {
     render() {
         return (
-          <div className="grid-item one-whole margin-top-xl">
-            <h3 className="font-xxl font-light text-center"><Translate value="diversity.headline"/></h3>
-            <div className="grid-item one-half" >
-              <GenderChart/>
-            </div>
-            <div className="grid-item one-half padding-top-l" >
-              <h6 className="font-l font-light four-fifths text-center"><Translate value="diversity.origin"/></h6>
-              <img className="four-fifths padding-top-s" src={diversityMap}/>
-            </div>
+          <div>
+              <div className="grid-item one-whole margin-top-xl">
+                <h3 className="font-xxl font-light text-center"><Translate value="diversity.headline"/></h3>
+                <Claim/>
+                <div className="grid-item one-half" >
+                  <GenderChart/>
+                </div>
+                <div className="grid-item one-half padding-top-l" >
+                  <h6 className="font-l font-light four-fifths text-center"><Translate value="diversity.origin"/></h6>
+                  <img className="four-fifths padding-top-s" src={diversityMap}/>
+                </div>
+              </div>
           </div>
         );
     }
