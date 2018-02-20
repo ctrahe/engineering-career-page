406c86e4c5ee1d9e103298cefbcb6a1fb4145b0b
mkrey <maya.krey@immobilienscout24.de>
2018-02-20 11:18:49 +0100

    DIT-69: html for all open positions

--- src/components/AllPositionsPage/index.jsx
+++ src/components/AllPositionsPage/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+
 import Page from '../Page';
+import './allPositions.css';
 
 class AllPositions extends React.Component {
 
   render() {
     return (
       <Page>
-        All open positions at Scout24
+        <h1 className="title font-h2 padding-vertical-l">Current Job Openings</h1>
+        <div className="grid gutter-xl">
+          <div className="grid-item palm-one-whole lap-one-half desk-one-third">
+            <div className="positions-container border padding-horizontal-m padding-vertical-l">
+              <p className="font-m">Business Development & Strategy</p>
+              <span className="font-xxl font-light padding-right-s">3</span><span className="font-s font-lightgray">positions</span>
+            </div>
+          </div>
+        </div>
       </Page>
     );
   }
