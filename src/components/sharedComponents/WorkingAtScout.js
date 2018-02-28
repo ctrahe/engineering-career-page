8e7880a689bbd7c663ef1211db4d33adc04e4a08
ikarydi <ioanna.karydi@scout24.com>
2018-02-28 22:20:45 +0100

    DIT-70: Remove duplicate content in footer

--- src/components/sharedComponents/WorkingAtScout.js
+++ src/components/sharedComponents/WorkingAtScout.js
@@ -42,19 +42,6 @@ class WorkingAtScout extends React.PureComponent {
         <div className="grid-item one-whole">
           { this.props.button }
         </div>
-        <div className="grid-item one-whole">
-          <div className="divider margin-vertical-l"/>
-          <div className="social-media-links margin-bottom-m">
-            <a target="_blank" href="https://www.linkedin.com/company/scout24-group" rel="noopener noreferrer" title="Linked In" className="linkedin" />
-            <a target="_blank" href="https://www.xing.com/companies/scout24holdinggmbh" rel="noopener noreferrer" title="Xing" className="xing" />
-            <a target="_blank" href="https://twitter.com/Scout24" rel="noopener noreferrer" title="Twitter" className="twitter" />
-            <a target="_blank" href="https://www.youtube.com/user/scout24" rel="noopener noreferrer" title="YouTube" className="youtube" />
-            <a target="_blank" href="https://www.facebook.com/Scout24" rel="noopener noreferrer" title="Facebook" className="facebook" />
-            <a target="_blank" href="https://www.instagram.com/scout24_official/" rel="noopener noreferrer" title="Instagram" className="instagram" />
-          </div>
-          <a className="block margin-bottom-s" target="_blank" href="http://blog.scout24.com/" rel="noopener noreferrer">Corporate Blog</a>
-          <a className="block" target="_blank" href="http://techblog.scout24.com/" rel="noopener noreferrer">Tech Blog</a>
-        </div>
       </div>
     );
   }
