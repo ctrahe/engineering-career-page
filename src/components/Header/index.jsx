e10504d7a6c68ecc815a7d3c03c6a656e7f39575
ikarydi <ioanna.karydi@scout24.com>
2018-02-28 13:54:24 +0100

    DIT-53: Add language highlighting and simplify toggling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,29 +4,37 @@ import { I18n } from 'react-i18nify';
 import logoScout24 from '../../images/scout24.png';
 import './header.css'
 
+class Header extends React.Component {
 
-const changeLocaleToGerman = () => {
-  I18n.setLocale('de', true);
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'en'
+    };
+  }
 
-const changeLocaleToEnglish = () => {
-  I18n.setLocale('en', true);
-};
+  changeLocale(selectedLocale) {
+    I18n.setLocale(selectedLocale, true);
+    this.state.selectedLanguage = selectedLocale;
+  };
 
-const Header = () => (
-  <div className="grid-item one-whole">
-    <div className="grid grid-flex grid-align-center padding-vertical-l padding-left-l padding-right-xxl header-line-height">
-      <div className="grid-item one-half">
-        <a href="http://www.scout24.com"><img src={logoScout24} className="logo" alt="logo" /></a>
-      </div>
-      <div className="grid-item one-half">
-        <div className="float-right languages">
-          <a onClick={() => changeLocaleToGerman()}>DE</a>
-          <a onClick={() => changeLocaleToEnglish()}>EN</a>
+  render() {
+    return (
+      <div className="grid-item one-whole">
+        <div className="grid grid-flex grid-align-center padding-vertical-l padding-left-l padding-right-xxl header">
+          <div className="grid-item one-half">
+            <a href="http://www.scout24.com"><img src={logoScout24} className="logo" alt="logo"/></a>
+          </div>
+          <div className="grid-item one-half">
+            <div className="float-right languages">
+              <a className={(this.state.selectedLanguage =='de' ? 'selected' : '')} onClick={() => this.changeLocale('de')}>DE</a>
+              <a className={(this.state.selectedLanguage =='en'  ? 'selected' : '')} onClick={() => this.changeLocale('en')}>EN</a>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Header;
