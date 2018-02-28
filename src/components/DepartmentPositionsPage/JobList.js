1273fb4b5b419150ad248acdd8894415024e60ec
ikarydi <ioanna.karydi@scout24.com>
2018-02-28 16:47:11 +0100

    DIT-70: Add open positions logic (WIP)

--- src/components/DepartmentPositionsPage/JobList.js
+++ src/components/DepartmentPositionsPage/JobList.js
@@ -12,7 +12,7 @@ class JobList extends Component {
         super(props);
         this.state = {
             jobAds: null,
-            department: 'all',
+            department: this.props.department,
             city: props.city ? props.city : 'all',
             company: 'all',
             position: 'all',
@@ -22,14 +22,12 @@ class JobList extends Component {
 
     componentWillMount() {
         getJobAds().then(jobs => {
-            console.log(jobs);
                 this.setState({...this.state, jobAds: jobs});
             }
         );
     }
 
     applyFilter(event)  {
-        console.log(event.target.name, event.target.value);
         if (event.target.name === 'department') {
             this.setState({ department: event.target.value })
         }
@@ -45,61 +43,54 @@ class JobList extends Component {
     }
 
     renderJobAds() {
-        if (!this.state.jobAds) {
-            return <div className="spinner">
-                        <div className="bounce1"></div>
-                        <div className="bounce2"></div>
-                        <div className="bounce3"></div>
-                    </div>;
-        }
-        const jobs = this.state.jobAds
-            .filter(jobAd => (
-                (
-                    (this.state.city === 'all' ? true : jobAd.location.name.includes(this.state.city)) ||
-                    (this.state.city === 'Munich' ? jobAd.location.name.includes('München') : false) ||
-                    (this.state.city === 'Vienna' ? jobAd.location.name.includes('Wien') : false)
-                ) &&
-                (
-                    (this.state.position === 'all' ? true :
-                        (this.state.position === 'parttime' ?
-                                //Filter Parttime
-                                (
-                                    (jobAd.title.toLowerCase().includes('intern')) ||
-                                        jobAd.title.toLowerCase().includes('praktik')
-                                ) :
-                                //Filter Fulltime
-                                (
-                                    !(jobAd.title.toLowerCase().includes('intern')) &&
-                                    !jobAd.title.toLowerCase().includes('praktik')
-                                )
-                        )
-                    )
-                ) &&
-                (this.state.department === 'all' ? true : jobAd.departments[0].name.includes(this.state.department)) &&
-                (
-                    this.state.company === 'all' ? true :
-                        this.state.company === 'Scout24' ? jobAd.metadata[0].value.toLowerCase() === this.state.company.toLowerCase() :
-                            jobAd.metadata[0].value.toLowerCase().includes(this.state.company.toLowerCase()))))
-            .map((jobAd) => (
-                <JobAdEntry jobAd={jobAd} key={jobAd.id}/>)
-            );
-        return jobs;
+      if (!this.state.jobAds) {
+          return <div className="spinner">
+                      <div className="bounce1"></div>
+                      <div className="bounce2"></div>
+                      <div className="bounce3"></div>
+                  </div>;
+      }
+      else {
+        return this.state.jobAds.jobs
+          .filter(jobAd => (
+            (
+              (this.state.city === 'all' ? true : jobAd.location.name.includes(this.state.city)) ||
+              (this.state.city === 'Munich' ? jobAd.location.name.includes('München') : false) ||
+              (this.state.city === 'Vienna' ? jobAd.location.name.includes('Wien') : false)
+            ) &&
+            (
+              (this.state.position === 'all' ? true :
+                  (this.state.position === 'parttime' ?
+                      //Filter Parttime
+                      (
+                        (jobAd.title.toLowerCase().includes('intern')) ||
+                        jobAd.title.toLowerCase().includes('praktik')
+                      ) :
+                      //Filter Fulltime
+                      (
+                        !(jobAd.title.toLowerCase().includes('intern')) &&
+                        !jobAd.title.toLowerCase().includes('praktik')
+                      )
+                  )
+              )
+            ) &&
+            (this.state.department === 'all' ? true : jobAd.departments[0].name.includes(this.state.department)) &&
+            (
+              this.state.company === 'all' ? true :
+                this.state.company === 'Scout24' ? jobAd.metadata[0].value.toLowerCase() === this.state.company.toLowerCase() :
+                  jobAd.metadata[0].value.toLowerCase().includes(this.state.company.toLowerCase()))))
+          .map((jobAd) => (
+            <JobAdEntry jobAd={jobAd} key={jobAd.id}/>));
+      }
     }
 
     render() {
-
         const filteredJobAds = this.renderJobAds();
 
         return (
             <div className="jobs-container">
-                <div className="job-info font-m palm-one-whole">
-                    <select className="filterField" onChange={ (event) => this.applyFilter(event) } name="position">
-                        <option selected value="all"><Translate value="filter.position.all"/></option>
-                        <option value="fulltime"><Translate value="filter.position.fullTime"/></option>
-                        <option value="parttime"><Translate value="filter.position.partTime"/></option>
-                    </select>
-                </div>
                 <div className="job-info palm-one-whole">
+                  <span className="filter-label">Team</span>
                   <select className="filterField" onChange={ (event) => this.applyFilter(event) } name="department">
                     <option selected value="all"><Translate value="filter.department.all"/></option>
                     <option value="Technology"><Translate value="Technology"/></option>
@@ -109,15 +100,8 @@ class JobList extends Component {
                     <option value="Product Management & UX"><Translate value="Product Management & UX"/></option>
                   </select>
                 </div>
-                <div className="job-info palm-one-whole">
-                  <select className="filterField" name="city"  onChange={ (event) => this.applyFilter(event) } value={this.state.city}>
-                    <option selected value="all"><Translate value="filter.cities.all"/></option>
-                    <option value="Berlin"><Translate value="filter.cities.berlin"/></option>
-                    <option value="Munich"><Translate value="filter.cities.munich"/></option>
-                    <option value="Vienna"><Translate value="filter.cities.vienna"/></option>
-                  </select>
-                </div>
                <div className="job-info palm-one-whole">
+                 <span className="filter-label">Company</span>
                  <select className="filterField" name="company"  onChange={ (event) => this.applyFilter(event) }>
                    <option selected value="all"><Translate value="filter.company.all"/></option>
                    <option value="AutoScout24"><Translate value="filter.company.as24"/></option>
@@ -125,14 +109,27 @@ class JobList extends Component {
                    <option value="Scout24"><Translate value="filter.company.s24"/></option>
                  </select>
               </div>
+              <div className="job-info palm-one-whole">
+                <span className="filter-label">Location</span>
+                <select className="filterField" name="city"  onChange={ (event) => this.applyFilter(event) } value={this.state.city}>
+                  <option selected value="all"><Translate value="filter.cities.all"/></option>
+                  <option value="Berlin"><Translate value="filter.cities.berlin"/></option>
+                  <option value="Munich"><Translate value="filter.cities.munich"/></option>
+                  <option value="Vienna"><Translate value="filter.cities.vienna"/></option>
+                </select>
+              </div>
+              <div className="job-info font-m palm-one-whole">
+                <span className="filter-label">Type of employment</span>
+                <select className="filterField" onChange={ (event) => this.applyFilter(event) } name="position">
+                  <option selected value="all"><Translate value="filter.position.all"/></option>
+                  <option value="fulltime"><Translate value="filter.position.fullTime"/></option>
+                  <option value="parttime"><Translate value="filter.position.partTime"/></option>
+                </select>
+              </div>
                 <ul className="jobs-list">
-
-
                     { filteredJobAds }
-
                 </ul>
             </div>
-
         );
     }
 
--- src/components/DepartmentPositionsPage/index.jsx
+++ src/components/DepartmentPositionsPage/index.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import Page from '../Page';
 import Cover from '../Cover';
+import JobList from "./JobList";
 
 class DepartmentPositions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      department: "Technology"
+    };
+  }
 
   render() {
+    console.log(this.props);
    return (
-     <Page cover={<Cover phrase={"TESTTSTTSTSTS"} />}>
-       Open positions in this department
+     <Page cover={<Cover phrase={this.state.department} />}>
+       <div className="grid-item one-whole">
+         <JobList department={this.state.department}/>
+       </div>
      </Page>
+
    );
   }
 
